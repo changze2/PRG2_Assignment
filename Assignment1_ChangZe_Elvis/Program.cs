a11ed883d4e3c8330d28c9edef7e3cb87130610e
cz <s10255850@connect.np.edu.sg>
2024-01-18 01:50:46 +0800

    minor changes

--- Assignment1_ChangZe_Elvis/Program.cs
+++ Assignment1_ChangZe_Elvis/Program.cs
@@ -2,28 +2,73 @@
 
 
 using Assignment1_ChangZe_Elvis;
+using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 
 Dictionary<int, Customer> customerDict = new Dictionary<int, Customer>();
 Queue <Order> goldOrderQueue = new Queue<Order>();
 Queue <Order> orderQueue = new Queue<Order>();
 
-using (StreamReader sr = new StreamReader("customers.csv"))
+InitCustomers();
+
+while (true)
 {
-    string? s = sr.ReadLine();
-    if (s != null) { }
-    while ((s = sr.ReadLine()) != null)
+    Menu();
+    try
     {
-        string[] line = s.Split(',');
-        string name = line[0];
-        int id = Convert.ToInt32(line[1]);
-        string dob = line[2];
-        Customer customer = new Customer(name, id, dob);
-        customer.Rewards = new PointCard { Tier = line[3], Points = Convert.ToInt32(line[4]) };
-        customer.Rewards.PunchCard = Convert.ToInt16(line[5]);
-        customerDict.Add(id, customer);
+        Console.Write("Enter option: ");
+        int option = Convert.ToInt16(Console.ReadLine().Trim());
+
+        if (option < 0 || option > 6)
+        {
+            throw new Exception();
+        }
+        if (option == 0)
+        {
+            Console.WriteLine("Program ended.");
+            break;
+        }
+        Console.WriteLine();
+        switch (option)
+        {
+            case 1:
+                CustomerInfo();
+                break;
+            case 2:
+                DisplayCurrentOrders();
+                break;
+            case 3:
+                RegisterCustomer();
+                break;
+        }
+        Console.WriteLine();
+    }
+    catch
+    {
+        Console.WriteLine("Invalid option entered. Please enter a valid option.\n");
     }
 }
+
+void InitCustomers()
+{
+    using (StreamReader sr = new StreamReader("customers.csv"))
+    {
+        string? s = sr.ReadLine();
+        if (s != null) { }
+        while ((s = sr.ReadLine()) != null)
+        {
+            string[] line = s.Split(',');
+            string name = line[0];
+            int id = Convert.ToInt32(line[1]);
+            string dob = line[2];
+            Customer customer = new Customer(name, id, dob);
+            customer.Rewards = new PointCard { Tier = line[3], Points = Convert.ToInt32(line[4]) };
+            customer.Rewards.PunchCard = Convert.ToInt16(line[5]);
+            customerDict.Add(id, customer);
+        }
+    }
+}
+
 
 void Menu()
 {
@@ -107,26 +152,32 @@ void RegisterCustomer()
             throw new ArgumentException("Name cannot be empty or whitespace.");
         }
         //This line of code will trigger if the name is null or whitespace
-        Console.Write("Enter customer id number (e.g 650992): ");
-        if (!int.TryParse(Console.ReadLine(), out int id) || id <= 0)
+
+        Console.Write("Enter customer id number (e.g 123456): ");
+        if (!int.TryParse(Console.ReadLine(), out int id) || id <= 0 || id.ToString().Length != 6 )
         {
-            throw new ArgumentException("Invalid customer ID. Please follow the examples (e.g 650992).");
+            throw new ArgumentException("Invalid customer ID. Please follow the examples (e.g 123456).");
+        }
+        else if (customerDict.ContainsKey(id))
+        {
+            throw new ArgumentException($"Sorry, {id} has already been registered to another customer.");
         }
         //This line of code reads a customer ID from the console, attempts to parse it into an integer using `int.TryParse`,
         //and checks if the parsing fails or if the parsed integer is not a positive value. If either condition is true,
         //it throws an `ArgumentException` with a message indicating that the customer ID is invalid and
-        //provides an example for the correct format (e.g., "650992").
-        Console.Write("Enter date of birth (DD/MM/YYYY): ");
+        //provides an example for the correct format (e.g., "123456").
+
+        Console.Write("Enter date of birth (DD-MM-YYYY): ");
         string dobString = Console.ReadLine();
-        if (!DateTime.TryParseExact(dobString, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dob))
+        if (!DateTime.TryParseExact(dobString, "d-M-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dob))
         {
-            throw new ArgumentException("Invalid date of birth format. Please use DD/MM/YYYY.");
+            throw new ArgumentException("Invalid date of birth format. Please use DD-MM-YYYY.");
         }
         //This condition checks if the DateTime.TryParseExact method fails to parse the dobString using the specified format "dd/MM/yyyy."
         //If the parsing fails, the method returns false, and the ! operator negates it, making the condition true.
         //In this case, if the date of birth format is invalid, the block inside the if statement is executed, and an ArgumentException is thrown.
 
-        Customer newCustomer = new Customer(name, id, dob.ToString("dd/MM/yyyy"));
+        Customer newCustomer = new Customer(name, id, dob.ToString());
         PointCard newPointCard = new PointCard(0, 0);
         newPointCard.Tier = "Ordinary";
         newCustomer.Rewards = newPointCard;
@@ -136,7 +187,7 @@ void RegisterCustomer()
     }
     catch (ArgumentException ex)
     {
-        Console.WriteLine($"Invalid input: {ex.Message}");
+        Console.WriteLine(ex.Message);
     }
     catch (Exception)
     {
@@ -155,40 +206,4 @@ void AppendToCsvFile(Customer customer)
     File.AppendAllLines(filePath, new[] { csvLine });
 }
 
-while (true)
-{
-    Menu();
-/*    try
-    {*/
-        Console.Write("Enter option: ");
-        int option = Convert.ToInt16(Console.ReadLine());
 
-        if (option < 0 || option > 6)
-        {
-            throw new Exception();
-        }
-        if (option == 0)
-        {
-            Console.WriteLine("Program ended.");
-            break;
-        }
-        Console.WriteLine();
-        switch (option)
-        {
-            case 1:
-                CustomerInfo();
-                break;
-            case 2:
-                DisplayCurrentOrders();
-                break;
-            case 3:
-                RegisterCustomer();
-                break;
-        }
-        Console.WriteLine();
-    //}
-/*    catch
-    {
-        Console.WriteLine("Invalid option entered. Please re-enter.\n");
-    }*/
-}
