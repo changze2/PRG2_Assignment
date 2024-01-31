fa1890a2a144d7bc5573788247bb59872b3952aa
LENOVO <LENOVO@DESKTOP-67VL0H2>
2024-01-31 16:53:49 +0800

    elvis

--- Assignment1_ChangZe_Elvis/Program.cs
+++ Assignment1_ChangZe_Elvis/Program.cs
@@ -959,11 +959,19 @@ void ProcessOrder(Order order)
     Console.WriteLine($"{customer.Name}'s current membership points: {membershipPoint}\n");
     if (customer.IsBirthday())
     {
-        // Calculate the final bill while having the most expensive ice cream cost $0.00
-        IceCream mostExpensiveIceCream = order.IceCreamList.OrderByDescending(icecream => 
-        icecream.CalculatePrice()).First();
-        totalPrice -= mostExpensiveIceCream.CalculatePrice();
-        Console.WriteLine($"It's the customer's birthday! The most expensive icecream is free of charge!");
+        bool check = customer.OrderHistory.Any(order => order.TimeReceived.Date == DateTime.Now.Date);
+        if (check)
+        {
+            Console.WriteLine("The customer has already redeemed the free birthday ice cream");
+        }
+        else
+        {
+            // Calculate the final bill while having the most expensive ice cream cost $0.00
+            IceCream mostExpensiveIceCream = order.IceCreamList.OrderByDescending(icecream =>
+            icecream.CalculatePrice()).First();
+            totalPrice -= mostExpensiveIceCream.CalculatePrice();
+            Console.WriteLine($"It's the customer's birthday! The most expensive icecream is free of charge!");
+        }
     }
     if (customer.Rewards.PunchCard >= 10 && !customer.IsBirthday())
     {
